891e7933ba8775a31dfa9072c492fed1e87baf88
daivedl <daived@live.com.ar>
2021-06-20 13:27:36 -0300

    parcial 2 Met-Sis Profesor Cortez

---
+++ src/database/migrations/20210620160344-create-product.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      stockMin: {
+        type: Sequelize.INTEGER
+      },
+      stockMax: {
+        type: Sequelize.INTEGER
+      },
+      brandId: {
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        type: Sequelize.INTEGER
+      },
+      sizeId: {
+        type: Sequelize.INTEGER
+      },
+      genderId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
